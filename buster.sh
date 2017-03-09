4cd8d3902ca2072f4a8f314092b4497a2b2ea76a
Shrikrishna Holla <shrikrishna.holla@gmail.com>
2017-03-09 06:42:47 +0530

    Fix xmls and robots.txt that still contain localhost:2368

--- buster.sh
+++ buster.sh
@@ -15,6 +15,12 @@ find static -name *.html -type f -exec sed -i -e 's#s\#http://blog.shrikrishnaho
 echo "Removing index.html from links"
 find static -name *.html -type f -exec sed -i -e 's#/index.html#/#g' {} \;
 
+echo "Fixing some xmls"
+find static -name *.xml -type f -exec sed -i -e 's#http://localhost:2368#http://blog.shrikrishnaholla.in#g' {} \;
+
+echo "Fixing robots.txt"
+find static -name robots.txt -type f -exec sed -i -e 's#http://localhost:2368#http://blog.shrikrishnaholla.in#g' {} \;
+
 if [ "$1" == "--deploy" ]; then
   buster deploy
 fi
